605e72683eb754b1845bb4cfd3bb5a55cb7371af
Kirill Bakhtiiarov <kirycha21@gmai.com>
2023-02-19 11:31:26 +0200

    Премер работы с http модулем

---
+++ workWithServer/http.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const fs = require("fs");
+let server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  if (req.url === "/") fs.createReadStream("./templates/index.html").pipe(res);
+  else if (req.url === "/about")
+    fs.createReadStream("./templates/about.html").pipe(res);
+  else  fs.createReadStream("./templates/error.html").pipe(res);
+});
+
+const PORT = 3000;
+let HOST = "localhost";
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server started at http://${HOST}:${PORT}`);
+});
